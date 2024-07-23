712d28c8c3a5b77faa62034bd6ff2f13c5f35854
Donghwa Shin <68676833+dhshin04@users.noreply.github.com>
2024-07-23 10:45:30 +0900

    Updated local path

--- parasite_egg_detection/s3_to_ec2.sh
+++ parasite_egg_detection/s3_to_ec2.sh
@@ -6,9 +6,9 @@
 # Move images in S3 to EC2 Instance
 S3_BUCKET="ieee-dataport"
 S3_PATH_TRAIN="competition/420/Chula-ParasiteEgg-11.zip"
-LOCAL_PATH_TRAIN="/home/ubuntu/data/trainingset/images/"
+LOCAL_PATH_TRAIN="/home/ubuntu/data/general_test/images/"
 S3_PATH_TEST="competition/420/Chula-ParasiteEgg-11_test.zip"
-LOCAL_PATH_TEST="/home/ubuntu/data/testset/images/"
+LOCAL_PATH_TEST="/home/ubuntu/data/general_dataset/images/"
 
 aws s3 cp s3://$S3_BUCKET/$S3_PATH_TRAIN $LOCAL_PATH_TRAIN
 aws s3 cp s3://$S3_BUCKET/$S3_PATH_TEST $LOCAL_PATH_TEST
